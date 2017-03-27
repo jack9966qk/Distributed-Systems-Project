9cedee57bb17617ca5ed531acd5590c67027c3b0
zhepingLiu <zheping.ProvidenceO@gmail.com>
2017-03-27 16:00:59 +1100

    Unit test for client and server

---
+++ src/test/java/com/allstars/unit_test/ClientTest.java
@@ -0,0 +1,116 @@
+package com.allstars.unit_test;
+
+import com.allstars.project1.Client;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Zheping on 2017/3/26.
+ */
+class ClientTest {
+
+    class ServiceThread extends Thread {
+        private DataInputStream inputStream;
+        private DataOutputStream outputStream;
+
+        public ServiceThread(Socket socket) {
+
+            try {
+                this.inputStream = new DataInputStream(socket.getInputStream());
+                this.outputStream = new DataOutputStream(socket.getOutputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run() {
+
+            try {
+                String inText = inputStream.readUTF();
+
+                if (inText.equals("quit()")) {
+                    System.out.println("Server quit.");
+                }
+
+                outputStream.writeUTF("Receive from client:" + inText);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+    }
+
+    @BeforeEach
+    void setUp() {
+        // set up the dummy server and create a Client instance
+        try {
+            ServerSocket socket = new ServerSocket(2333);
+            System.out.println("Server listen to connections.");
+
+            int i=0;
+            Socket clientSocket = socket.accept();
+            System.out.println("Received connection: " + ++i);
+
+            ServiceThread service = new ServiceThread(clientSocket);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void share() {
+        String[] share;
+    }
+
+    @Test
+    void query() {
+        String[] query;
+    }
+
+    @Test
+    void fetch() {
+        String[] fetch;
+    }
+
+    @Test
+    void exchange() {
+        String[] exchange;
+    }
+
+    @Test
+    void getOptions() {
+        String[] testCmd = new String[] {};
+    }
+
+    @Test
+    void makeResourceFromCmd() {
+    }
+
+    @Test
+    void connectToServer() {
+        String[] connection = new String[] {"-host", "localhost", "-port", "2333"};
+        new Client().main(connection);
+    }
+
+//    @Test
+//    void main() {
+//    }
+
+}
---
+++ src/test/java/com/allstars/unit_test/ServerTest.java
@@ -0,0 +1,130 @@
+package com.allstars.unit_test;
+
+import com.allstars.project1.Server;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Zheping on 2017/3/25.
+ */
+class ServerTest {
+
+    class ServiceThread extends Thread {
+
+        @Override
+        public void run() {
+            String[] args = new String[] {"-port", "2333", "-secret", "JackBigLeg"};
+            Server.main(args);
+        }
+    }
+
+    class Client {
+        Socket s = null;
+        DataInputStream in = null;
+        DataOutputStream out = null;
+
+        public void sendRequest(String request) {
+            try {
+                s = new Socket("localhost", 2333);
+                in = new DataInputStream(s.getInputStream());
+                out = new DataOutputStream(s.getOutputStream());
+                // send a publish request
+                out.writeUTF(request);
+                String response = in.readUTF();
+                System.out.println(response);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public Client() throws IOException {
+        }
+    }
+
+    @BeforeEach
+    void setUp() {
+        new ServiceThread().start();
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+        Client c = null;
+
+        String publish = "{'command': 'PUBLISH', " +
+                "'resource': {" +
+                "'name': 'UOM', " +
+                "'tags': ['web'], " +
+                "'description': 'university of melbourne', " +
+                "'uri': 'http:\\/\\/www.unimelb.edu.au', " +
+                "'channel': '', " +
+                "'owner': '', " +
+                "'ezserver': null}}";
+
+        String remove = "{'command': 'REMOVE', " +
+                "'resource': {" +
+                "'name': 'UOM', " +
+                "'tags': ['web'], " +
+                "'description': 'university of melbourne', " +
+                "'uri': 'http:\\/\\/www.unimelb.edu.au', " +
+                "'channel': '', " +
+                "'owner': '', " +
+                "'ezserver': null}}";
+
+        String query = "{'command': 'QUERY', " +
+                "'relay': true, " +
+                "'resourceTemplate': {" +
+                "'name': '', " +
+                "'tags': [], " +
+                "'description': '', " +
+                "'uri': '', " +
+                "'channel': '', " +
+                "'owner': '', " +
+                "'ezserver': null}}";
+
+        String fetch = "{'command': 'FETCH', " +
+                "'resourceTemplate': {'name': '', " +
+                "'tags': [], " +
+                "'description': '', " +
+                "'uri': 'file:\\/\\/\\/\\/home\\/aaron\\/EZShare\\/ezshare.jar', " +
+                "'channel': '', " +
+                "'owner': '', " +
+                "'ezserver': null}}";
+
+        String exchange = "{'command': 'EXCHANGE', " +
+                "'serverList': [" +
+                "{" +
+                "'hostname': 'localhost', " +
+                "'port': 2333" +
+                "}," +
+                "{" +
+                "'hostname': '115.146.85.24', " +
+                "'port': 2333" +
+                "}" +
+                "]}";
+
+        try {
+            c = new Client();
+            c.sendRequest(exchange);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
