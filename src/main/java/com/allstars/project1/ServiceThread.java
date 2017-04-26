1ea61505673e70157d8b6ec79a6a4a5d246d8bee
fina <374694363@qq.com>
2017-04-26 15:44:56 +1000

    Update ServiceThread.java

--- src/main/java/com/allstars/project1/ServiceThread.java
+++ src/main/java/com/allstars/project1/ServiceThread.java
@@ -62,7 +62,7 @@ public class ServiceThread extends Thread {
      private void checkTemplate(Resource template) throws ServerException {
         Resource r = new Resource(null, null, null, template.getUri(),
                 template.getChannel(), null, null);
-        if (r.getUri().isEmpty()) {            // uri is empty
+        if (r.getUri().isEmpty()|r.getChannel().isEmpty()) {   // uri or uri is empty
             throw new ServerException("missing resourceTemplate");
         } else if (!isFile(r.getUri())) {      // not a file
             throw new ServerException("invalid resourceTemplate");
